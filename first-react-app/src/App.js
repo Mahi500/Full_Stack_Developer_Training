66f5fc94b6056c52f9a0349f0c25153917b896b4
Bommu Mahendar Reddy <bommumahireddy046@gmail.com>
2022-11-22 12:30:31 +0530

    reactjs

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,9 +3,38 @@ import User from './User';
 import Employee from './Employee';
 import EmployeeWithFunction from './EmployeeWithFunction';
 import Company from './Company';
+import { useState } from 'react';
 
 function App() {
 
+  let msg="hello good morning";
+
+  let result= `hi ${msg}`;
+  console.log(result);
+
+  let address={
+
+    state:"Telangana",
+    district:"Warangal",
+    village:"cheemakurthi",
+    pincode:522590
+  }
+
+  let {state,district,village,pincode}=address;
+
+  console.log(state);
+  console.log(district);
+  console.log(village);
+  console.log(pincode);
+
+const ages=[50,60,70];
+
+console.log(ages[0],ages[1],ages[2]);
+
+const [first,second,third]= ages;
+console.log(first,second,third);
+
+const [color,setColor]= useState(80);
   const age=60;
   return (
     
--- practice2.js
+++ practice2.js
@@ -44,3 +44,8 @@ async function userandrole(){
 }
 
 userandrole();
+
+let str1="hello";
+let str2="world";
+
+console.log(`${str1} ${str2}`);
